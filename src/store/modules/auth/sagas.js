9e437c229a386a60feb8fc8fd108976ea2f054d1
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-05 03:56:02 -0300

    feature: signup and login requesting

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,9 +1,10 @@
 import { takeLatest, all, put, call } from 'redux-saga/effects';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import history from '~/services/history';
 import api from '~/services/api';
 
 import { signInSuccess, signFailure } from './actions';
+import { sign } from 'crypto';
 
 function* signIn({ payload }) {
   try {
@@ -25,9 +26,31 @@ function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (error) {
-    toast.error('Falha na autenticação, verifique seus dados.')
+    toast.error('Falha na autenticação, verifique seus dados.');
     yield put(signFailure());
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);

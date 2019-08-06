8daa60e9053e4198d127d173c4e69ee62129775c
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-06 03:27:05 -0300

    feature: notifications

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -36,6 +36,9 @@ export const NotificationList = styled.div`
   background: rgba(0, 0, 0, 0.6);
   border-radius: 4px;
   padding: 15px 5px;
+  display: ${props => {
+    return props.visible ? 'bock' : 'none';
+  }};
 
   &::before {
     content: '';
@@ -70,8 +73,10 @@ export const Notification = styled.div`
   }
 
   time {
+    display: block;
     font-size: 12px;
     opacity: 0.6;
+    margin-bottom: 5px;
   }
 
   button {
@@ -80,8 +85,6 @@ export const Notification = styled.div`
     background: none;
     color: ${lighten(0.2, '#7159c1')};
     padding: 0 5px;
-    margin: 0 5px;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
   }
 
   ${props =>
@@ -94,6 +97,7 @@ export const Notification = styled.div`
         height: 8px;
         background: #ff892e;
         border-radius: 50%;
+        margin-left: 10px;
       }
     `}
 `;
